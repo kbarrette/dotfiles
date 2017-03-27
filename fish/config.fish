82e3f694e6fa73acce5900f942c67f6fd8561800
Keith Barrette <etterrab@gmail.com>
2017-03-27 15:55:05 -0400

    Add fish config

--- fish/config.fish
+++ fish/config.fish
@@ -13,3 +13,8 @@ fish_hybrid_key_bindings
 status --is-interactive; and source (rbenv init -|psub)
 
 set fish_user_paths $HOME/bin /usr/local/sbin
+
+if test -e ~/.config/fish/local.fish
+  source ~/.config/fish/local.fish
+end
+
