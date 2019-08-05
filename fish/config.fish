c96adb86bd741baf9f7e9274328179296bcb1783
Keith Barrette <etterrab@gmail.com>
2019-08-05 08:13:45 -0400

    Improved modified-specs

--- fish/config.fish
+++ fish/config.fish
@@ -26,7 +26,7 @@ if test -e ~/.config/fish/local.fish
 end
 
 function modified-specs
-  set -l files (git status --porcelain | string sub -s4 | string replace --regex '^app' 'spec' | string replace --regex '(?<!_spec).rb' '_spec.rb' | grep '^spec' | while read -la line; if test -e $line; echo $line; end; end | string join ' ')
+  set -l files (git status --porcelain | string sub -s4 | string replace --regex '^app' 'spec' | string replace --regex '(?<!_spec).rb' '_spec.rb' | grep '^spec' | grep -v '^spec/fixtures' | uniq | while read -la line; if test -e $line; echo $line; end; end | string join ' ')
 
   if test -n "$files"
     echo $files
